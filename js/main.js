11a258c39e2abd7c4e2929d99f9e3a2ad78abede
Ugolty <latry.ugo@gmail.com>
2023-04-16 12:08:51 +0200

    oui

--- js/main.js
+++
@@ -1,5 +0,0 @@
-let menu = document.querySelector('.menu');
-               toggle.onclick = function(){
-                    toggle.classList.toggle('active')
-                    menu.classList.toggle('active')
-               }
